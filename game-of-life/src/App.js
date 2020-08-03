c4d47094f9bed1b46ec0f2bee354c961caae9e94
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2020-08-03 14:08:15 -0400

    added new code for start component and and grid layout to simplify canvas and grid canvas render

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,154 +1,111 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
-import Grid from './components/Grid';
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import produce from 'immer';
+// import Grid from './components/Grid';
 import Buttons from './components/Buttons';
 import Modal from './components/Modal';
 // import triangles from '../src/images/triangles.jpg';
 import './App.scss';
 
 
+const numRows = 25;
+const numCols = 25;
+let speed = 1000;
 
-function App() {
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const App = () => {
 
   // asign state to array for row and array for col - fill initial state with zeros
-  const [gridDisplay, setGridDisplay] = useState(() => new Array(rows).fill().map(() => new Array(cols).fill(Math.random() > 0.25 ? true : false)));
+  const [gridDisplay, setGridDisplay] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows
+  });
   const [generation, setGeneration] = useState(0);
-  const [interval, setInterval] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
 
 
-
-  let rows = 30;
-  let cols = 50;
-  let speed = 100;
-
-  const operations = [
-    [0, 1],
-    [0, -1],
-    [1, -1],
-    [-1, 1],
-    [1, 1],
-    [-1, -1],
-    [1, 0],
-    [-1, 0]
-  ];
-
-
-  //update a array "copy" of grid then set gridDisplay state to update state
-  const selectCell = (x, y) => {
-    const gridCopy = gridDisplay.map((row, rowKey) => {
-      if (rowKey === x) {
-        return row.map((cell, colKey) => {
-          if (colKey === y) {
-            return cell === 0 ? 1 : 0;
-          } else {
-            return cell;
-          }
-        });
-      } else {
-        return row;
+  const useInterval = (callback, delay, started) => {
+    const savedCallback = useRef();
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
       }
-    });
-    setGridDisplay(gridCopy);
+      let id = null;
+      if (delay !== null && started) {
+        id = setInterval(tick, delay);
+        return () => {
+          clearInterval(id);
+        };
+      } else {
+        if (id) {
+          clearInterval(id);
+        }
+      }
+    }, [callback, delay, started]);
   };
-
-  // const selectCell = (row, col) => {
-  //   let gridCopy = arrayClone(gridDisplay);
-  //   //find exact cell that was clicked then set to opposite
-  //   gridCopy[row][col] = !gridDisplay[row][col];
-  //   setGridDisplay(gridCopy);
-  //   // console.log("SELECTED", gridCopy[row][col])
-  //   console.log("SELECTED", gridDisplay)
-  // }
 
 
   const randomCells = () => {
-
-    //set up copy of grid
-    let gridCopy = arrayClone(gridDisplay);
-    //nested for loops iterate through grid decide state of each cell
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        //assign grid buffer to 50% random cell selection
-        if (Math.floor(Math.random() * 4) === 1) {
-          gridCopy[i][j] = true
-        }
-        // gridCopy[i][j] = Math.floor(Math.random() * 2)
-      }
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => Math.random() > 0.7 ? 1 : 0));
     }
-    console.log("SET GRID DISPLAY", gridDisplay)
-    setGridDisplay(gridCopy)
-    // return gridCopy
-  }
-
-  // useEffect initiates state with random alive and dead cells
-  useEffect(() => {
-    console.log('random ceeeells', gridDisplay)
-    return (startButton())
-  }, [])
-
-
-  let startButton = useCallback(() => {
-    if (!isRunning) {
-      setIsRunning(true)
-      runningRef.current = true;
-      // start()
-      // setTimeout(start, 1000)
-      console.log("please work", gridDisplay)
-    } else {
-      setIsRunning(false)
-    }
-  })
-
-  let stopButton = () => {
-    let id = setInterval(interval);
-    setInterval(id);
-    return clearInterval(id);
+    setGridDisplay(rows)
   }
 
   //set grid to initial new array, set generations back to zero
-  let reset = () => {
-    let grid = new Array(rows).fill().map(() => new Array(cols).fill(false));
-    setGridDisplay(grid)
+  const reset = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    setGridDisplay(rows)
     setGeneration(0)
   }
   //set speed to 1000
-  let slow = () => {
-    speed = 1000;
-    startButton();
+  const slow = () => {
+    // speed = 1000;
+    setIsRunning(isRunning, speed = 1000)
   }
   //set speed to 100
-  let fast = () => {
-    speed = 100;
-    startButton();
+  const fast = () => {
+    setIsRunning(isRunning, speed = 300)
   }
-
-
-
-  const runningRef = useRef(isRunning);
-  runningRef.current = isRunning
 
 
   //function that starts the game, sets the edge cases
   //accounts for neighbors, sets game logic based off neighbors
   const start = useCallback(() => {
-    if (!isRunning.current) {
-      return;
-    }
-    setGridDisplay((gridDisplay => {
-      return selectCell(gridDisplay, gridCopy => {
-        for (let i = 0; i < rows; i++) {
-          for (let j = 0; j < cols; j++) {
+    setGridDisplay((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
             let aliveNeighbors = 0;
             operations.forEach(([x, y]) => {
               const newI = i + x;
               const newJ = j + y;
-              if (newI >= 0 && newI < rows && newJ >= 0 && newJ < cols) {
-                aliveNeighbors += gridDisplay[newI][newJ];
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                aliveNeighbors += g[newI][newJ];
               }
-            });
+            })
             if (aliveNeighbors < 2 || aliveNeighbors > 3) {
               gridCopy[i][j] = 0;
-            } else if (gridDisplay[i][j] === 0 && aliveNeighbors === 3) {
+            } else if (g[i][j] === 0 && aliveNeighbors === 3) {
               gridCopy[i][j] = 1;
             }
           }
@@ -156,33 +113,49 @@ function App() {
       });
     });
 
-    setTimeout(start, 1000);
   }, []);
 
+  useInterval(
+
+    () => start(),
+    300, isRunning
+  );
 
   return (
-    <div className="App">
-      <div className="star-contianer">
-        {/* <div className="starlite"><img src={triangles} alt="" style={{ width: "300px", zIndex: "9", top: "15px" }} /></div> */}
-        <div className="title"><h2>Conway's Game of Life</h2></div>
-        {/* <div className="starlite"><img src={triangles} alt="" style={{ width: "300px" }} /></div> */}
-      </div>
+    <>
       <div className="components">
-        <Grid gridDisplay={gridDisplay} rows={rows} cols={cols} selectCell={selectCell} />
-        <Buttons startButton={startButton} stopButton={stopButton} reset={reset} slow={slow} fast={fast} randomCells={randomCells} isRunning={isRunning} runningRef={runningRef} />
-        <Modal />
+        <div className="grid" style={{
+          width: "510px",
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}>
+          {gridDisplay.map((rows, i) =>
+            rows.map((col, j) =>
+
+              <div
+                className="cell"
+                key={`${i}` - `${j}`}
+                onClick={() => {
+                  const newGrid = produce(gridDisplay, gridCopy => {
+                    if (!isRunning) {
+                      gridCopy[i][j] = gridDisplay[i][j] ? 0 : 1;
+                    }
+                  })
+                  setGridDisplay(newGrid);
+
+                }}
+
+                style={
+                  gridDisplay[i][j] ? { "margin": "0 auto", "width": 0, "height": 0, "borderLeft": "7px solid transparent", "borderRight": "7px solid transparent", "borderBottom": "15px solid rgb(84, 114, 204)" } : undefined
+                }
+              />
+            ))}
+        </div>
+        <Buttons setIsRunning={setIsRunning} isRunning={isRunning} randomCells={randomCells} reset={reset} slow={slow} fast={fast} />
       </div>
-      <div><h3 className="title">Generations: {generation}</h3></div>
-    </div>
-  );
+    </>
+  )
 }
 
-//function to clone 2d arrays
-let arrayClone = (arr) => {
-  //set to string and parse clone arrays inside of arrays
-  return JSON.parse(JSON.stringify(arr));
-}
 
 export default App;
-
-
--- game-of-life/src/components/Buttons.js
+++ game-of-life/src/components/Buttons.js
@@ -3,23 +3,26 @@ import React from 'react';
 const Buttons = (props) => {
 
     return (
-        <div className="btn-display">
-            <button className="btn-style" onClick={props.startButton, props.runningRef.current = true} >
-                {props.isRunning ? "stop" : "start"}
+        <>
+            <div className="btn-display">
+                <button className="btn-style" onClick={() => {
+                    props.setIsRunning(!props.isRunning)
+                }}>
+                    {props.isRunning ? "stop" : "start"}
+                </button>
+                <button className="btn-style" onClick={props.reset} >
+                    Reset
             </button>
-        <button className="btn-style" onClick={props.reset} >
-            Reset
+                <button className="btn-style" onClick={props.slow}>
+                    Slow
             </button>
-        <button className="btn-style" onClick={props.slow}>
-            Slow
+                <button className="btn-style" onClick={props.fast}>
+                    Fast
             </button>
-        <button className="btn-style" onClick={props.fast}>
-            Fast
-            </button>
-        <button className="btn-style" onClick={props.randomCells}>
-            Random Input
-            </button>
-        </div >
+                <button className="btn-style" onClick={props.randomCells}>
+                    Random Input
+            </button></div>
+        </>
     )
 }
 export default Buttons;
